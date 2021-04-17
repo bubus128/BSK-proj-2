7eb8ad7feed5eb77b1396c7d864274eccfdad603
bubus128 <j.swislocki@outlook.com>
2021-04-17 15:03:15 +0200

    small fixes

--- Main.py
+++ Main.py
@@ -1,9 +1,8 @@
 import tkinter as tk
 
-
 window = tk.Tk()
-lable=tk.Label(text="enter your query");
-entry= tk.Text()
+lable = tk.Label(text="enter your query")
+entry = tk.Text()
 output = tk.Label(
     width=90,
     height=15,
@@ -15,16 +14,17 @@ button = tk.Button(
     height=5,
     bg="white",
 )
+
+
 def execute(event):
-    query=entry.get("1.0", tk.END)
+    query = entry.get("1.0", tk.END)
     print(query)
 
-if __name__ == '__main__':
 
-    button.bind("<Button-1>",execute)
+if __name__ == '__main__':
+    button.bind("<Button-1>", execute)
     lable.pack()
     entry.pack()
     button.pack()
     output.pack()
     window.mainloop()
-
