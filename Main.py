2b9d013b6a832dc3871fa16d3aa502f71ba6480c
bubus128 <j.swislocki@outlook.com>
2021-04-17 15:01:07 +0200

    query reading

--- Main.py
+++ Main.py
@@ -1,20 +1,27 @@
 import tkinter as tk
 
+
+window = tk.Tk()
+lable=tk.Label(text="enter your query");
+entry= tk.Text()
+output = tk.Label(
+    width=90,
+    height=15,
+    bg="white"
+)
+button = tk.Button(
+    text="Execute",
+    width=10,
+    height=5,
+    bg="white",
+)
+def execute(event):
+    query=entry.get("1.0", tk.END)
+    print(query)
+
 if __name__ == '__main__':
-    window = tk.Tk()
-    lable=tk.Label(text="enter your query");
-    entry= tk.Text()
-    output = tk.Label(
-        width=90,
-        height=15,
-        bg="white"
-    )
-    button = tk.Button(
-        text="Execute",
-        width=10,
-        height=5,
-        bg="white",
-    )
+
+    button.bind("<Button-1>",execute)
     lable.pack()
     entry.pack()
     button.pack()
