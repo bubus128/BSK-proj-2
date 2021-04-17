92c194d78ce5fb39b33b62b7fb5079b77e2d9588
bubus128 <j.swislocki@outlook.com>
2021-04-17 14:43:02 +0200

    Change framework to TKinter

--- Main.py
+++ Main.py
@@ -1,18 +1,23 @@
-import PySimpleGUI as Sg
+import tkinter as tk
 
-input_column = [[Sg.Text("enter your query")],
-                [Sg.Multiline(size=(50, 20), key='textbox')],
-                [Sg.Button('Execute'), Sg.Button('Close Window')]]
-output_column = [[Sg.Text("output")],
-                 [Sg.Multiline(size=(50, 20), key='textboxa')]]
-layout = [[Sg.Column(input_column),
-            Sg.VSeperator(),
-           Sg.Column(output_column)]]
-window = Sg.Window("Query executor", layout)
-while True:
-    event, values = window.read()
-    if event == "Execute":
-        pass
-    elif event == "Close Window" or event == Sg.WIN_CLOSED:
-        break
-window.close()
+if __name__ == '__main__':
+    window = tk.Tk()
+    lable=tk.Label(text="enter your query");
+    entry= tk.Text()
+    output = tk.Label(
+        width=90,
+        height=15,
+        bg="white"
+    )
+    button = tk.Button(
+        text="Execute",
+        width=10,
+        height=5,
+        bg="white",
+    )
+    lable.pack()
+    entry.pack()
+    button.pack()
+    output.pack()
+    window.mainloop()
+
